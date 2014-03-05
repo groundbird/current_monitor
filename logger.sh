be35790149994c656245b3423a2033420699b895
GB in quiet-desktop <no@mail>
2014-03-05 17:04:42 +0900

    Format adc.py and add .gitignore

--- logger.sh
+++ logger.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DATADIR=/home/gb/adc16/data
+DATADIR=/home/gb/current_monitor/data
 FILENAME=`date '+%Y%m%d%H%M%S'`
-CMD=/home/gb/adc16/adc.py
+CMD=/home/gb/current_monitor/adc.py
 
 ${CMD} | tee ${DATADIR}/${FILENAME}.dat
